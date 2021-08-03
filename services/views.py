070e36e01d7b484c380172e5efca4d65b6709240
INYEOB <inshining97@gmail.com>
2021-08-04 00:15:16 +0900

    Feat:[카테고리별 리스트 나열]

--- services/views.py
+++ services/views.py
@@ -1,24 +1,37 @@
 from django.shortcuts import render
-from .models import Service
-
-# TODO:카테고리 별로 db에 요청해야함
-def services_list(request):
-    services = Service.objects.all()
-    ctx = {'services': services}
-    return render(request, 'services/list.html', context=ctx)
+from .models import *
+from django.http import HttpResponse
 
 def main(request):
     ctx = {"main": main}
     return render(request, 'services/main.html', context=ctx)
 
-def category_list(request, slug):
-    services = Service.objects.filter(category.slug==slug)
-    ctx = {'services': services}
+# 전체 보기 페이지
+def services_list(request):
+    services = Service.objects.all()
+    categories = Category.objects.all()
+    ctx = {
+        'services': services,
+        'categories':categories,
+        }
     return render(request, 'services/list.html', context=ctx)
 
+# 카테고리별 페이지 보기
+def category_list(request, slug):
+    services = Service.objects.filter(category__slug__contains = slug)
+    categories = Category.objects.all()
+
+    ctx = {
+        'services': services,
+        'categories':categories,
+        }
+    return render(request, 'services/list.html', context=ctx)
 
 def services_detail(request, pk):
     service = Service.objects.get(id=pk)
     ctx = {'service': service}
     return render(request, 'services/detail.html', context=ctx)
+
+
+
 
