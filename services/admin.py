78a05e731ed721be208cedca20fd433ea8e3a82a
INYEOB <inshining97@gmail.com>
2021-08-03 19:03:43 +0900

    Feat:[서비스명+한줄소개_표기]

--- services/admin.py
+++ services/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Service, Category
+from .models import Service, Category, SubCategory
 
 
 @admin.register(Category)
+class IdeaAdmin(admin.ModelAdmin):
+    pass 
+
+@admin.register(SubCategory)
 class IdeaAdmin(admin.ModelAdmin):
     pass 
 @admin.register(Service)
