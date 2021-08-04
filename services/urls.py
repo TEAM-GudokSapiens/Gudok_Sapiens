e82ab69771eda52f7dfbbbf60edfdce2d5e5432b
INYEOB <inshining97@gmail.com>
2021-08-04 11:05:44 +0900

    Feat:[클래스 기반 검색 기능 구현 시도, 함수 기반 검색 기능으로 변경할 예정]

--- services/urls.py
+++ services/urls.py
@@ -5,6 +5,7 @@ app_name = 'services'
 
 urlpatterns = [
     path('main/', views.main, name='main'),
+    path('service_list/', views.ServiceListView.as_view(), name='service_list_view'),
     path('list/', views.services_list, name='services_list'),
     path('list/<slug:slug>/', views.category_list, name='category_list'),
     path('detail/<int:pk>/', views.services_detail, name='services_detail'),
--- services/views.py
+++ services/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import *
-from django.http import HttpResponse
+from django.contrib import messages 
+from django.db.models import Q
+from django.views.generic import View, ListView
 
 def main(request):
     ctx = {"main": main}
@@ -32,6 +34,45 @@ def services_detail(request, pk):
     ctx = {'service': service}
     return render(request, 'services/detail.html', context=ctx)
 
+
+class ServiceListView(ListView):
+    model = Service
+    template_name = 'services/search_result.html'
+    context_object_name = 'service_list_view'
+
+    def get_queryset(self):
+        search_keyword = self.request.GET.get('q', '')
+        search_type = self.request.GET.get('type', '')
+        service_list = Service.objects.all()
+
+        if search_keyword :
+            if len(search_keyword) > 1:
+                if search_type == 'all':
+                    search_service_list = service_list.filter(Q (name__icontains=search_keyword) | Q(intro__icontains=search_keyword) | Q(content__icontains=search_keyword))
+                elif search_type == 'name_content':
+                    search_service_list = service_list.filter(Q (name__icontains=search_keyword) | Q(content__icontains=search_keyword))
+                elif search_type == 'name':
+                    search_service_list = service_list.filter(name__icontains=search_keyword)
+                elif search_type == 'intro':
+                    search_service_list = service_list.filter(intro__icontains=search_keyword)
+                elif search_type == 'content':
+                    search_service_list = service_list.filter(content__icontains=search_keyword)
+                
+                return search_service_list
+            else:
+                messages.error(self.request, '검색어는 2글자 이상 입력해주세요.')
+        return service_list
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        search_keyword = self.request.GET.get('q', '')
+        search_type = self.request.GET.get('type', '')
+
+        if len(search_keyword) > 1:
+            context['q'] = search_keyword
+        context['type'] = search_type
+
+        return context
 
 
 
