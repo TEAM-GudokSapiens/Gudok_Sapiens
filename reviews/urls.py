90a848a7fdd0f51889711691f007657971ac4635
sungeuni0208 <sungeuni0208@naver.com>
2021-08-06 17:56:15 +0900

    Fix: [ReviewCreateView 수정]

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'reviews'
 
 urlpatterns = [
-    path('create/', views.ReviewCreateView.as_view(), name='create')
+    path('create/', views.ReviewCreateView.as_view(),
+         name='create'),
+    #     path('create/<int:pk>', views.reviews_create,
+    #          name='create'),
+    #
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from services.models import Service
+from django.shortcuts import render, redirect
 from .models import Review
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView
@@ -8,20 +9,27 @@ from reviews.forms import ReviewCreateForm
 class ReviewCreateView(CreateView):
     model = Review
     form_class = ReviewCreateForm
-    success_url = reverse_lazy('services:services_detail')
     template_name = 'reviews/create.html'
 
     def form_valid(self, form):
-        temp_review = form.save(commit=False)
-        temp_review.target = self.object.service
-        temp_review.user = self.request.user
-        # temp_review.target = form.data.get('target')
-
-        return super().form_valid(form)
+        form.instance.user = self.request.user
+        form.instance.target = Service.objects.get(pk=1)
+        return super(ReviewCreateView, self).form_valid(form)
 
     def get_success_url(self):
         return reverse('services:services_detail', kwargs={'pk': self.object.target.id})
 
-# def create(request):
-#     ctx = {"create": create}
-#     return render(request, 'revieews/create.html', context=ctx)
+# def reviews_create(request, pk):
+#     if request.method == "POST":
+#         form = ReviewCreateForm(request.POST)
+#         if form.is_valid():
+#             review_form = form.save(commit=False)
+#             review_form.user = request.user
+#             review_form.target = Service.objects.get(pk=pk)
+#             review_form.save()
+#             return redirect("services:services_list")
+#     else:
+#         form = ReviewCreateForm()
+
+#     ctx = {"form": form}
+#     return render(request, "reviews/create.html", ctx)
