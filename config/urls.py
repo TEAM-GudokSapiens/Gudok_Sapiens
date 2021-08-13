2af6a8a684f2ffb22ff896a6db4d032ed4d14f44
Bebop <tjr50999@naver.com>
2021-08-13 16:48:53 +0900

    Design: [main화면 프론트 구현 완료]

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ from ckeditor_uploader import views as views_ckeditor
 from django.views.decorators.cache import never_cache
 
 urlpatterns = [
-    path('', lambda req: redirect('services:services_list'), name='root'),
+    path('', lambda req: redirect('services:main'), name='root'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('services/', include('services.urls', namespace="services")),
@@ -16,8 +16,9 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('community/', include('community.urls')),
     path('likes/', include('likes.urls')),
-        
+
     path('upload/', login_required(views_ckeditor.upload), name='ckeditor_upload'),
-    path('browse/', never_cache(login_required(views_ckeditor.browse)), name='ckeditor_browse'),
+    path('browse/', never_cache(login_required(views_ckeditor.browse)),
+         name='ckeditor_browse'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
