5982b043d30be5110171d9beb4b75f77d6e7b5c5
Sungeun <sungeuni0208@naver.com>
2021-08-16 23:59:53 +0900

    Merge: [Merge branch 'feature/community_board_2' into develop]

--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate, update_session_auth_hash
@@ -9,7 +8,6 @@ from django.core.paginator import Paginator
 from django.utils.decorators import method_decorator
 from django.views.generic import UpdateView, DeleteView, FormView, CreateView, View
 from services.models import Service
-from reviews.models import Review
 from django.core.paginator import Paginator
 from django.contrib.auth.tokens import PasswordResetTokenGenerator, default_token_generator
 from django.core.exceptions import PermissionDenied
@@ -48,11 +46,15 @@ class Signup(CreateView):
         return super().get(request, *args, **kwargs)
 
     def get_success_url(self):
+        # messages.success(self.request, "회원가입 성공.")
         self.request.session['register_auth'] = True
         messages.success(
             self.request, '회원님의 입력한 Email 주소로 인증 메일이 발송되었습니다. 인증 후 로그인이 가능합니다.')
+        # return settings.LOGIN_URL
         return reverse('users:register_success')
 
+    def form_valid(self, form):
+        self.object = form.save()
 
         send_mail(
             '{}님의 회원가입 인증메일 입니다.'.format(self.object.user_id),
@@ -104,7 +106,6 @@ class LoginView(FormView):
     template_name = 'users/login.html'
     form_class = LoginForm
     success_url = '/services/main/'
-    ctx = {}
 
     def form_valid(self, form):
         user_id = form.cleaned_data.get("user_id")
@@ -113,12 +114,8 @@ class LoginView(FormView):
 
         if user is not None:
             self.request.session['user_id'] = user_id
-            if user.is_active:
-                login(self.request, user)
-            else:
-                ctx.update(
-                    {'messages': "회원님의 입력한 Email 주소로 인증 메일이 발송되었습니다. 인증 후 로그인이 가능합니다."})
-                return render(self.request, 'users/login.html', ctx)
+            login(self.request, user,
+                  backend="django.contrib.auth.backends.ModelBackend")
 
         return super().form_valid(form)
 
@@ -196,16 +193,17 @@ def dibs_list(request):
 
 
 def reviews_list(request):
-    reviews_list = Review.objects.filter(user=request.user.id)
+    reviews_list = Service.objects.filter(
+        review__user=request.user.id).distinct()
     # 한 페이지 당 담을 수 있는 객체 수를 정할 수 있음
     paginator = Paginator(reviews_list, 10)
     page = request.GET.get('page')
-    reviews = paginator.get_page(page)
+    services = paginator.get_page(page)
 
     ctx = {
-        'reviews': reviews,
+        'services': services,
     }
-    return render(request, 'users/reviews_list.html', context=ctx)
+    return render(request, 'users/dibs_list.html', context=ctx)
 
 
 # 이용약관 동의
@@ -224,7 +222,6 @@ class AgreementView(View):
             messages.info(request, "약관에 모두 동의해주세요.")
             return render(request, 'users/agreement.html')
 
-# 아이디 찾기
 
 # 아이디 찾기
 @method_decorator(logout_message_required, name='dispatch')
@@ -259,70 +256,70 @@ class RecoveryPwView(View):
             return render(request, self.template_name, {'form_pw': form_pw, })
 
 
-    return render(request, 'users/password_reset.html', {'form': reset_password_form})
-        reset_password_form = CustomSetPasswordForm(request.user)
-
-    else:
-            request.session['auth'] = session_user
-        else:
-            logout(request)
-            return redirect('users:login')
-            logout(request)
-            messages.success(request, "비밀번호 변경완료! 변경된 비밀번호로 로그인하세요.")
-            user = reset_password_form.save()
-        if reset_password_form.is_valid():
-
-        reset_password_form = CustomSetPasswordForm(request.user, request.POST)
-
-              backend="django.contrib.auth.backends.ModelBackend")
-        login(request, current_user,
-        current_user = User.objects.get(user_id=session_user)
-
-        session_user = request.session['auth']
-    if request.method == 'POST':
-            raise PermissionDenied
-        if not request.session.get('auth', False):
-    if request.method == 'GET':
-def auth_pw_reset_view(request):
-@logout_message_required
-# 비밀번호찾기 새비밀번호 등록
-
-
-    return HttpResponse(json.dumps({"result": user.user_id}, cls=DjangoJSONEncoder), content_type="application/json")
-
-    request.session['auth'] = user.user_id
-    user.save()
-    user.auth = ""
-    # login(request, user)
-    user = User.objects.get(user_id=user_id, auth=input_auth_num)
-    input_auth_num = request.POST.get('input_auth_num')
+# 비밀번호찾기 AJAX 통신
+def ajax_find_pw_view(request):
     user_id = request.POST.get('user_id')
-    # if request.method=='POST' and 'auth_confirm' in request.POST:
-def auth_confirm_view(request):
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    result_pw = User.objects.get(user_id=user_id, name=name, email=email)
 
+    if result_pw:
+        auth_num = email_auth_num()
+        result_pw.auth = auth_num
+        result_pw.save()
+        send_mail(
+            '{}님의 비밀번호 찾기 인증메일입니다.'.format(user_id),
+            message='',
+            recipient_list=[email],
+            from_email='david90907@naver.com',
+            html_message=render_to_string('users/recovery_email.html', {
+                'auth_num': auth_num,
+            })
+        )
+        # print('이메일전송완료')
+    # print(auth_num)
+    return HttpResponse(json.dumps({"result": result_pw.user_id}, cls=DjangoJSONEncoder), content_type="application/json")
 
 # 비밀번호찾기 인증번호 확인
 
-    return HttpResponse(json.dumps({"result": result_pw.user_id}, cls=DjangoJSONEncoder), content_type="application/json")
-    # print(auth_num)
-        # print('이메일전송완료')
-        )
-            })
-                'auth_num': auth_num,
-            html_message=render_to_string('users/recovery_email.html', {
-            from_email='david90907@naver.com',
-            recipient_list=[email],
-            '{}님의 비밀번호 찾기 인증메일입니다.'.format(user_id),
-            message='',
-        send_mail(
-        result_pw.save()
-        result_pw.auth = auth_num
-        auth_num = email_auth_num()
-    if result_pw:
 
-    result_pw = User.objects.get(user_id=user_id, name=name, email=email)
-    email = request.POST.get('email')
-    name = request.POST.get('name')
+def auth_confirm_view(request):
+    # if request.method=='POST' and 'auth_confirm' in request.POST:
     user_id = request.POST.get('user_id')
-def ajax_find_pw_view(request):
-# 비밀번호찾기 AJAX 통신
+    input_auth_num = request.POST.get('input_auth_num')
+    user = User.objects.get(user_id=user_id, auth=input_auth_num)
+    # login(request, user)
+    user.auth = ""
+    user.save()
+    request.session['auth'] = user.user_id
+
+    return HttpResponse(json.dumps({"result": user.user_id}, cls=DjangoJSONEncoder), content_type="application/json")
+
+
+# 비밀번호찾기 새비밀번호 등록
+@logout_message_required
+def auth_pw_reset_view(request):
+    if request.method == 'GET':
+        if not request.session.get('auth', False):
+            raise PermissionDenied
+
+    if request.method == 'POST':
+        session_user = request.session['auth']
+        current_user = User.objects.get(user_id=session_user)
+        login(request, current_user,
+              backend="django.contrib.auth.backends.ModelBackend")
+
+        reset_password_form = CustomSetPasswordForm(request.user, request.POST)
+
+        if reset_password_form.is_valid():
+            user = reset_password_form.save()
+            messages.success(request, "비밀번호 변경완료! 변경된 비밀번호로 로그인하세요.")
+            logout(request)
+            return redirect('users:login')
+        else:
+            logout(request)
+            request.session['auth'] = session_user
+    else:
+        reset_password_form = CustomSetPasswordForm(request.user)
+
+    return render(request, 'users/password_reset.html', {'form': reset_password_form})
