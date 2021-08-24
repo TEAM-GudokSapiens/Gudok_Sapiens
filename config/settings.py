d44f5324d24ef388efd027cf705ca14d2d064b2d
songkyung <yysk_915@naver.com>
2021-08-24 16:23:45 +0900

    Fix: [공지사항 프로필image 수정]

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # -------------------------------------------바꿔주기---------------------------------------------
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -165,8 +165,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
